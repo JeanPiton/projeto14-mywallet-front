4a5388112243e9aab84d199163d5e9fcf5695128
JeanPiton <jean.orpiton@gmail.com>
2023-07-05 18:10:10 -0300

    Implement Sign up

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios"
 
 export default function SignUpPage() {
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [confirm,setConfirm] = useState("")
+
+  function submit(event){
+    event.preventDefault()
+    if(password!=confirm){
+      return alert("Senhas não são iguais")
+    }
+    const data = {name, email, password}
+    axios.post(`${import.meta.env.VITE_API_URL}/users`,data)
+    .then(()=>alert("Cadastro realizado com sucesso"))
+    .catch(e=>alert(e.response.data))
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={submit}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input placeholder="Nome" type="text" required value={name} onChange={e=>setName(e.target.value)}/>
+        <input placeholder="E-mail" type="email" required value={email} onChange={e=>setEmail(e.target.value)}/>
+        <input placeholder="Senha" type="password" autocomplete="new-password" required value={password} onChange={e=>setPassword(e.target.value)}/>
+        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" required value={confirm} onChange={e=>setConfirm(e.target.value)}/>
         <button>Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
